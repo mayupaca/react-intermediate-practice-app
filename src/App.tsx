0ff1ceeb35f31747f8c5da48e7ba30c90da22c16
Mayu Suzumura <47727645+mayupaca@users.noreply.github.com>
2022-05-16 14:09:15 -0400

    Apply global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
-import "./styles.css";
+import { Button, ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme/theme";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <ChakraProvider theme={theme}>
+      <Button colorScheme="teal">Button</Button>
+    </ChakraProvider>
+  );
 }
