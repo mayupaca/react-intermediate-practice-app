523e8de1463137a04d580f433907a2e9fa9433db
Mayu Suzumura <47727645+mayupaca@users.noreply.github.com>
2022-05-21 14:27:08 -0400

    User list component

--- src/components/pages/UserManegement.tsx
+++ src/components/pages/UserManegement.tsx
@@ -1,5 +1,18 @@
+import { Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
+import { UserCard } from "../organisms/user/UserCard";
+
 export const UserManegement: VFC = memo(() => {
-  return <p>User Manegement Page</p>;
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard
+          imageUrl="https://source.unsplash.com/random"
+          userName="Tomatoronto"
+          fullName="Jessica Canada"
+        />
+      </WrapItem>
+    </Wrap>
+  );
 });
